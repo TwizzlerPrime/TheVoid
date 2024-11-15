26c4a5fbc1213eb54e122bf338ba5468ab6ba29f
iremide <IAdewumi@smail.pembinatrails.ca>
2024-11-15 14:25:56 -0600

    Massive overhaul and beginning reaper boss fight soon

--- thevoid.sh
+++ thevoid.sh
@@ -16,17 +16,25 @@ display_status() {
 
 echo "A silhouette of the Reaper dawns upon you, and he asks: 'Do you wish to tresspass unto my territory?'"
 sleep 2
+
+while true; do
 read -p "Make your move. (yes/no):" proceed
 
 if [[ $proceed == "no" ]]; then
 	echo "Coward, leave this place and never return."
 	exit 0
+	break
 elif [[ $proceed == "yes" ]]; then
 	echo "I pity you and your confidence, good luck."
 	display_status
 	echo ""
+	break
+else
+       echo "Are you lost? (try answering the question)"
 fi
+ done
 sleep 2
+
 echo "As you enter what seems to be a cavern, you spot a portal, a collapsing cave, and .. a book?"
 sleep 2
 echo "What will you inspect?"
@@ -38,7 +46,7 @@ if [[ $choice == "portal" ]]; then
 	echo "You approach the portal, but you get dizzy and are whisked away to a different section of the void."
 	sleep 1
 	echo "You lost energy!"
-Energy=$((Energy - 10))
+       Energy=$((Energy - 10))
        display_status
 	break
 
@@ -74,7 +82,7 @@ sanctuary_encounter() {
     local spell_energy=20
 while true; do
 
-	read -p "Suddenly, pulsating, weaponized energy barely misses you; a Darkheart has spotted you! What now? (sword, spell, pulsegun)" defense
+read -p "Suddenly, pulsating, weaponized energy barely misses you; a Darkheart has spotted you! What now? (sword, spell, pulsegun)" defense
 
 if [[ $defense == "sword" && $Energy -ge $sword_energy ]]; then
 	echo "When the Darkheart attacks, you parry the attack and deflect it back, defeating it."
@@ -99,21 +107,44 @@ display_status
 sleep 3
 echo "The Darkheart might have some interesting loot, but its body is volatile."
 sleep 1
-read -p "Do you scavenge it? (yes/no)" scavenge
 
 while true; do
+read -p "Do you scavenge it? (yes/no)" scavenge
+
   if [[ $scavenge == "no" ]]; then
 	echo "You move on, leaving it behind.."
 	break
   elif [[ "$scavenge" == "yes" ]]; then
-	echo "You're overwhelmed with a Darkheart's power, it strengthens you."
-	Energy=$((Energy + 60))
-	Health=$((Health + 100))
-	echo "You have recieved a massive bost in stats!"
-	break
-   
+
+	if (( RANDOM % 2 == 0)); then
+		echo "The Darkheart explodes! You take damage."
+		Health=$((Health - 50))
+		break
+	else
+		echo "You're overwhelmed with a Darkheart's power, it strengthens you."
+		Energy=$((Energy + 60))
+		Health=$((Health + 100))
+		echo "You have recieved a massive bost in stats!"
+		break
+      fi
 else
 	echo "You sit down and contemplate.. (pick)"
   fi
 done
 display_status
+
+
+reaper_encounter() {
+	local reaper_health=200
+	local reaper_damage=30
+	local reaper_lifesteal=20
+
+	local player_health=$Health
+	local player_energy=$Energy
+
+echo "You've wandered in my domain for far too long, it's time i kick you out."
+echo "Your soul is MINE."
+}
+
+sleep 2
+reaper_encounter
